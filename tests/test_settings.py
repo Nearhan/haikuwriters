4e2b15198236ef18bfbda79f55fe95fc6c639f03
Jeff May <jeff.may@audaxhealth.com>
2013-02-19 23:28:50 -0500

    Updated test names to get picked up by the test runner.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,5 +3,7 @@ from os import path
 # Import all standard settings (required)
 from haikuwriters.settings import *
 
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
 TEST_DISCOVER_TOP_LEVEL = path.dirname(path.dirname(__file__))
 TEST_DISCOVER_ROOT = path.join(TEST_DISCOVER_TOP_LEVEL, 'tests')
